f0f48bf0534e27712bad9e41df535663a472482f
shirohoo <shirohoo4102@gmail.com>
2021-08-23 23:25:08 +0900

    feat: 팩토리 메서드 오버로딩

--- src/main/java/com/ark/inflearnback/config/model/HttpResponse.java
+++ src/main/java/com/ark/inflearnback/config/model/HttpResponse.java
@@ -3,6 +3,7 @@ package com.ark.inflearnback.config.model;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -25,7 +26,19 @@ public class HttpResponse<T> {
         return new HttpResponse<T>(responseCode, responseMessage);
     }
 
+    public static <T> HttpResponse<T> of(final HttpStatus httpStatus, final String responseMessage) {
+        return new HttpResponse<T>(parseStr(httpStatus.value()), responseMessage);
+    }
+
+    public static <T> HttpResponse<T> of(final HttpStatus httpStatus) {
+        return new HttpResponse<T>(parseStr(httpStatus.value()), httpStatus.getReasonPhrase());
+    }
+
     public static <T> HttpResponse<T> of(final String responseCode, final String responseMessage, final T responseBody) {
         return new HttpResponse<T>(responseCode, responseMessage, responseBody);
+    }
+
+    private static String parseStr(final int number) {
+        return String.valueOf(number);
     }
 }

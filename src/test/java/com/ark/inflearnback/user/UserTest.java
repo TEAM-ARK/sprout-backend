433de40c91f10842bc7c768806936112b355b934
Changhun-H <hch4102@hanpass.com>
2021-08-23 10:04:11 +0900

    test: 깨진 테스트 코드 수정

--- src/test/java/com/ark/inflearnback/user/UserTest.java
+++ src/test/java/com/ark/inflearnback/user/UserTest.java
@@ -2,6 +2,7 @@ package com.ark.inflearnback.user;
 
 import com.ark.inflearnback.domain.user.User;
 import com.ark.inflearnback.domain.user.UserRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,21 +16,20 @@ public class UserTest {
     @Autowired
     UserRepository userRepository;
 
-
-//    @Test
-//    @DisplayName("빌더 패턴을 사용한 회원 객체 생성 테스트")
-//    void useBuilder() {
-//        User user = User.builder()
-//                .name("민철이")
-//                .email("smc5236@naver.com")
-//                .password("1234")
-//                .isSubscribed(true)
-//                .build();
-//
-//        assertThat(user.getEmail()).isEqualTo("smc5236@naver.com");
-//        assertThat(user.getRole()).isEqualTo(Role.GENERAL_USER);
-//        assertThat(user.getIsSubscribed()).isTrue();
-//    }
+    //    @Test
+    //    @DisplayName("빌더 패턴을 사용한 회원 객체 생성 테스트")
+    //    void useBuilder() {
+    //        User user = User.builder()
+    //                .name("민철이")
+    //                .email("smc5236@naver.com")
+    //                .password("1234")
+    //                .isSubscribed(true)
+    //                .build();
+    //
+    //        assertThat(user.getEmail()).isEqualTo("smc5236@naver.com");
+    //        assertThat(user.getRole()).isEqualTo(Role.GENERAL_USER);
+    //        assertThat(user.getIsSubscribed()).isTrue();
+    //    }
 
     @Test
     @DisplayName("Spring Data JPA 테스트")
@@ -40,8 +40,8 @@ public class UserTest {
         User savedUser1 = userRepository.save(user1);
         User savedUser2 = userRepository.save(user2);
 
-        assertThat(userRepository.findAll().size()).isEqualTo(2);
-        assertThat(userRepository.findById(savedUser1.getId()).get().getName()).isEqualTo("민철");
-        assertThat(userRepository.findById(savedUser2.getId()).get().getName()).isEqualTo("윤지");
+        Assertions.assertThat(userRepository.findAll().size()).isEqualTo(2);
+        Assertions.assertThat(userRepository.findById(savedUser1.getId()).get().getName()).isEqualTo("민철");
+        Assertions.assertThat(userRepository.findById(savedUser2.getId()).get().getName()).isEqualTo("윤지");
     }
 }

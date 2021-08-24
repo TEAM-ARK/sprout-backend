64da3f156393a337324585926f8be95e55cb6ba3
shirohoo <shirohoo4102@gmail.com>
2021-08-24 11:22:26 +0900

    feat: ApplicationTest

--- src/test/java/com/ark/inflearnback/ApplicationTest.java
+++ src/test/java/com/ark/inflearnback/ApplicationTest.java
@@ -1,4 +1,13 @@
 package com.ark.inflearnback;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class ApplicationTest {
+    @Test
+    void boot() throws Exception{
+    }
 }

a269466061d07b61c89ae1f637a8fd6e64304eaa
shirohoo <71188307+shirohoo@users.noreply.github.com>
2021-08-24 12:19:53 +0900

    ⚙ 협업툴 설정까지 마무리 (#34)
    * feat: _jira_link.yml
    
    * feat: pr-badge.yml
    
    * refactor: jira-link.yml
    
    * fix: syntax error
    
    * fix: 주석제거
    
    * Update pr-badge.yml
    
    * AIB-5 test
    
    * AIB-5 test
    
    * AIB-5 test
    
    * ARK-1 TEST
    
    * Update pr-badge.yml
    
    * ARK-1 (#32)
    
    * Move pr-badge.yml
    
    * ARK-1
    
    * Create codeql-analysis.yml
    
    * Update gradle.yml
    
    * Update gradle.yml
    
    * feat: spring.properties
    
    * refactor: rename HttpLogCustomRepository.java
    
    * feat: ApplicationTest
    
    * fix: jacocoTestCoverageVerification 임시 비활성화
    
    * Update gradle.yml
    
    * Update gradle.yml
    
    Co-authored-by: MinChul-Son <smc5236@naver.com>

--- src/main/java/com/ark/inflearnback/config/logger/http/HttpLogRepository.java
+++ src/main/java/com/ark/inflearnback/config/logger/http/HttpLogRepository.java
@@ -2,4 +2,4 @@ package com.ark.inflearnback.config.logger.http;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HttpLogRepository extends JpaRepository<HttpLog, Long>, HttpLogCustomRepository {}
+public interface HttpLogRepository extends JpaRepository<HttpLog, Long>, HttpLogQueryRepository {}

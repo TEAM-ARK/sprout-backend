85fd9dcf0f458ef4066a1f0f6075f4ce85306d36
shirohoo <shirohoo4102@gmail.com>
2021-08-25 23:32:02 +0900

    프로젝트 원상복구, @SpringBootTest 제거

--- src/test/java/com/ark/inflearnback/ApplicationTest.java
+++ src/test/java/com/ark/inflearnback/ApplicationTest.java
@@ -1,13 +1,4 @@
 package com.ark.inflearnback;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-@SpringBootTest
 class ApplicationTest {
-    @Test
-    void boot() throws Exception{
-    }
 }

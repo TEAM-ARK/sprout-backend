37a6ab00499134d955b72524db3ba931aef01737
shirohoo <shirohoo4102@gmail.com>
2021-08-26 20:58:14 +0900

    refactor: PermitAllFilter

--- src/main/java/com/ark/inflearnback/domain/security/filter/PermitAllFilter.java
+++ src/main/java/com/ark/inflearnback/domain/security/filter/PermitAllFilter.java
@@ -7,68 +7,54 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PermitAllFilter extends FilterSecurityInterceptor {
-
     private static final String FILTER_APPLIED = "__spring_security_filterSecurityInterceptor_filterApplied";
+
     private final boolean observeOncePerRequest = true;
     private final List<RequestMatcher> permitAllRequestMatchers = new ArrayList<>();
 
-    public PermitAllFilter(String... permitAllResources) {
+    public PermitAllFilter(final String... permitAllResources) {
         for (String resource : permitAllResources) {
             permitAllRequestMatchers.add(new AntPathRequestMatcher(resource));
         }
     }
 
-    public void invoke(FilterInvocation fi) throws IOException, ServletException {
-        if (
-                (fi.getRequest() != null)
-                        &&
-                        (fi.getRequest().getAttribute(FILTER_APPLIED) != null)
-                        &&
-                        observeOncePerRequest
-        ) {
+    public void invoke(final FilterInvocation fi) throws IOException, ServletException {
+        if ((Objects.nonNull(fi.getRequest())) && (fi.getRequest().getAttribute(FILTER_APPLIED) != null) && observeOncePerRequest) {
             fi.getChain().doFilter(fi.getRequest(), fi.getResponse());
         }
         else {
-            if (fi.getRequest() != null && observeOncePerRequest) {
+            if (Objects.nonNull(fi.getRequest()) && observeOncePerRequest) {
                 fi.getRequest().setAttribute(FILTER_APPLIED, Boolean.TRUE);
             }
-
-            InterceptorStatusToken token = beforeInvocation(fi);
-
+            final InterceptorStatusToken token = beforeInvocation(fi);
             try {
                 fi.getChain().doFilter(fi.getRequest(), fi.getResponse());
             }
             finally {
                 super.finallyInvocation(token);
             }
-
             super.afterInvocation(token, null);
         }
     }
 
     @Override
-    protected InterceptorStatusToken beforeInvocation(Object object) {
+    protected InterceptorStatusToken beforeInvocation(final Object object) {
         boolean permitAll = false;
-        HttpServletRequest request = ((FilterInvocation) object).getRequest();
-
         for (RequestMatcher requestMatcher : permitAllRequestMatchers) {
-            if (requestMatcher.matches(request)) {
+            if (requestMatcher.matches(((FilterInvocation) object).getRequest())) {
                 permitAll = true;
                 break;
             }
         }
-
         if (permitAll) {
             return null;
         }
-
         return super.beforeInvocation(object);
     }
-
 }

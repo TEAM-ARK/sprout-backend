d7266bde169668c5ade17d176982d8b5cb83f00e
lsj8367 <lsj8367@naver.com>
2021-08-27 23:07:10 +0900

    feat: Properties 숨김처리 설정

---
+++ src/main/java/com/ark/inflearnback/config/jasypt/JasyptConfig.java
@@ -0,0 +1,32 @@
+package com.ark.inflearnback.config.jasypt;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Autowired(required = false)
+    private JasyptProperties jasyptProperties;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(jasyptProperties.getPassword());
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}

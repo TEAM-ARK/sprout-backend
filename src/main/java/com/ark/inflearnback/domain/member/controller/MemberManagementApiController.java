7d5b5fc21c61301ea8e589c8339d840035a45a4c
shirohoo <shirohoo4102@gmail.com>
2021-08-27 22:45:39 +0900

    feature: 회원가입 API 컨트롤러 작성

--- src/main/java/com/ark/inflearnback/domain/member/controller/MemberManagementApiController.java
+++ src/main/java/com/ark/inflearnback/domain/member/controller/MemberManagementApiController.java
@@ -1,8 +1,12 @@
 package com.ark.inflearnback.domain.member.controller;
 
+import com.ark.inflearnback.config.model.HttpResponse;
 import com.ark.inflearnback.domain.member.dto.SignUpRequestDto;
+import com.ark.inflearnback.domain.member.service.MemberManagementService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +19,12 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberManagementApiController {
-    @PostMapping("/")
-    public void signUp(@Valid @RequestBody final SignUpRequestDto request){
+    private final MemberManagementService memberManagementService;
 
+    @PostMapping
+    public ResponseEntity<?> signUp(@Valid @RequestBody final SignUpRequestDto request) {
+        memberManagementService.signUp(request);
+        return ResponseEntity.ok(HttpResponse.of(HttpStatus.OK, "회원가입 완료."));
     }
 }
 

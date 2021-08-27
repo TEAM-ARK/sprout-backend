bb77fd1f682e13ec98c028e99ec93ede9da186a7
shirohoo <shirohoo4102@gmail.com>
2021-08-27 16:46:52 +0900

    feature: 컨테이너를 사용하겠다는 의미의 @EnableContainers 작성

---
+++ src/test/java/com/ark/inflearnback/annotation/EnableContainers.java
@@ -0,0 +1,16 @@
+package com.ark.inflearnback.annotation;
+
+import com.ark.inflearnback.extension.MySQL80Extension;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@ExtendWith(MySQL80Extension.class)
+public @interface EnableContainers {
+}

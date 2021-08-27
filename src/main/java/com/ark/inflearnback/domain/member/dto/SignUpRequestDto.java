b3dbbec7faa468d1474bb6d4138163758ab704fc
shirohoo <shirohoo4102@gmail.com>
2021-08-27 20:52:26 +0900

    feature: 회원 가입 요청 객체에 유효성 검사 추가

--- src/main/java/com/ark/inflearnback/domain/member/dto/SignUpRequestDto.java
+++ src/main/java/com/ark/inflearnback/domain/member/dto/SignUpRequestDto.java
@@ -1,17 +1,19 @@
-package com.ark.inflearnback.domain.security.dto;
+package com.ark.inflearnback.domain.member.dto;
 
 import lombok.Getter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 public class SignUpRequestDto {
     @Email
-    @NotNull
+    @NotNull(message = "이메일을 입력하세요.")
     private final String email;
 
-    @NotNull
+    @NotNull(message = "비밀번호를 입력하세요.")
+    @Pattern(regexp = "^((?!.*[\\s])(?=.*[A-Z])(?=.*\\d).{8,15})", message = "비밀번호는 공백 없이 영문/숫자/특수문자를 조합해 8~15자리여야 합니다.")
     private final String password;
 
     private SignUpRequestDto(final String email, final String password) {

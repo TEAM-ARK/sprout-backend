489c9374ac89919b707b7d390f783505e56ab962
shirohoo <shirohoo4102@gmail.com>
2021-08-27 16:47:10 +0900

    test: 커스텀 확장이 제대로 동작하는지 테스트 코드 작성

--- src/test/java/com/ark/inflearnback/domain/security/repository/MemberRepositoryTest.java
+++ src/test/java/com/ark/inflearnback/domain/security/repository/MemberRepositoryTest.java
@@ -1,14 +1,14 @@
 package com.ark.inflearnback.domain.security.repository;
 
-import com.ark.inflearnback.annotation.MySQLContainers;
-import com.ark.inflearnback.annotation.QuerydslTest;
+import com.ark.inflearnback.annotation.DataQueryJpaTest;
+import com.ark.inflearnback.annotation.EnableContainers;
 import com.ark.inflearnback.domain.security.model.Member;
 import com.ark.inflearnback.domain.security.model.Role;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-@QuerydslTest
-@MySQLContainers
+@DataQueryJpaTest
+@EnableContainers
 class MemberRepositoryTest {
     private final RoleRepository roleRepository;
     private final MemberRepository memberRepository;

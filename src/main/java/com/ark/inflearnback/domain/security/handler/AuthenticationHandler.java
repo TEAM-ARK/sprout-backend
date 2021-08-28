47da060420e3f936f44ce562d58997617373392f
MinChul-Son <smc5236@naver.com>
2021-08-28 22:18:01 +0900

    feature : 로그인 성공 handler

---
+++ src/main/java/com/ark/inflearnback/domain/security/handler/AuthenticationHandler.java
@@ -0,0 +1,17 @@
+package com.ark.inflearnback.domain.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationHandler implements AuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}

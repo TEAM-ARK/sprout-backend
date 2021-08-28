8f316f8605990c3731cb23ef59d6f5a355a05cff
shirohoo <shirohoo4102@gmail.com>
2021-08-29 02:08:51 +0900

    feature: SecurityConfig에 로그인 컴포넌트를 등록

--- src/main/java/com/ark/inflearnback/domain/security/SecurityConfig.java
+++ src/main/java/com/ark/inflearnback/domain/security/SecurityConfig.java
@@ -1,25 +1,27 @@
 package com.ark.inflearnback.domain.security;
 
 import com.ark.inflearnback.domain.security.factory.UrlResourcesMapFactoryBean;
-import com.ark.inflearnback.domain.security.filter.JsonLoginFilter;
 import com.ark.inflearnback.domain.security.filter.PermitAllFilter;
+import com.ark.inflearnback.domain.security.filter.RestLoginProcessingFilter;
 import com.ark.inflearnback.domain.security.filter.UrlFilterInvocationSecurityMetadataSource;
-import com.ark.inflearnback.domain.security.handler.AuthenticationHandler;
+import com.ark.inflearnback.domain.security.handler.CustomAuthenticationFailureHandler;
+import com.ark.inflearnback.domain.security.handler.CustomAuthenticationSuccessHandler;
 import com.ark.inflearnback.domain.security.provider.CustomAuthenticationProvider;
 import com.ark.inflearnback.domain.security.repository.MemberRepository;
 import com.ark.inflearnback.domain.security.service.CustomUserDetailsService;
 import com.ark.inflearnback.domain.security.service.SecurityResourceService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.access.vote.RoleHierarchyVoter;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -33,12 +35,15 @@ import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@Order(1)
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -46,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final SecurityResourceService securityResourceService;
     private final MemberRepository memberRepository;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
@@ -60,30 +66,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http
+                .csrf().disable()
                 .httpBasic().disable()
-                .authorizeRequests(authorize ->
-                        authorize.anyRequest().authenticated()
+
+                .authorizeRequests(
+                        authorize -> authorize.anyRequest().authenticated()
                                 .expressionHandler(expressionHandler())
                 )
-                .formLogin().disable()
-                .addFilterBefore(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class)
-                .addFilterAt(getAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
 
-    protected JsonLoginFilter getAuthenticationFilter() {
-        JsonLoginFilter filter = new JsonLoginFilter();
-        try {
-            filter.setFilterProcessesUrl("/api/v1/member/login");
-            filter.setAuthenticationManager(this.authenticationManagerBean());
-            filter.setUsernameParameter("email");
-            filter.setPasswordParameter("password");
-            filter.setAuthenticationSuccessHandler(new AuthenticationHandler());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return filter;
+                .formLogin().disable()
+
+                .addFilterAt(restLoginProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(customFilterSecurityInterceptor(), FilterSecurityInterceptor.class);
     }
+    //
+    //    protected JsonLoginFilter getAuthenticationFilter() {
+    //        JsonLoginFilter filter = new JsonLoginFilter();
+    //        try {
+    //            filter.setFilterProcessesUrl("/api/v1/member/login");
+    //            filter.setAuthenticationManager(this.authenticationManagerBean());
+    //            filter.setUsernameParameter("email");
+    //            filter.setPasswordParameter("password");
+    //            filter.setAuthenticationSuccessHandler(new AuthenticationHandler());
+    //        } catch (Exception e) {
+    //            e.printStackTrace();
+    //        }
+    //        return filter;
+    //    }
 
     @Bean
     public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
@@ -145,5 +155,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public RestLoginProcessingFilter restLoginProcessingFilter() throws Exception {
+        RestLoginProcessingFilter restLoginProcessingFilter = new RestLoginProcessingFilter();
+        restLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
+        restLoginProcessingFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
+        restLoginProcessingFilter.setAuthenticationFailureHandler(authenticationFailureHandler());
+        return restLoginProcessingFilter;
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler(objectMapper);
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new CustomAuthenticationFailureHandler(objectMapper);
     }
 }

9235519b7f41826107eb934458b35832e061a869
shirohoo <shirohoo4102@gmail.com>
2021-08-29 02:08:24 +0900

    feature: 로그인 성공 핸들러 작성

---
+++ src/main/java/com/ark/inflearnback/domain/security/handler/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,29 @@
+package com.ark.inflearnback.domain.security.handler;
+
+import com.ark.inflearnback.config.model.HttpResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+    private final ObjectMapper objectMapper;
+
+    public CustomAuthenticationSuccessHandler(final ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void onAuthenticationSuccess(final HttpServletRequest request, final HttpServletResponse response, final Authentication authentication) throws IOException, ServletException {
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        objectMapper.writeValue(response.getWriter(), HttpResponse.of(HttpStatus.OK));
+    }
+}

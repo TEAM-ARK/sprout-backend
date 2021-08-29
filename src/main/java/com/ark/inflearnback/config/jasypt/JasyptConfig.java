15a98f2daa858fb06dae94c569732d3727748c4d
lsj8367 <lsj8367@naver.com>
2021-08-29 14:06:08 +0900

    refactor: 겹치는 부분 메서드로 추출

--- src/main/java/com/ark/inflearnback/config/jasypt/JasyptConfig.java
+++ src/main/java/com/ark/inflearnback/config/jasypt/JasyptConfig.java
@@ -3,7 +3,6 @@ package com.ark.inflearnback.config.jasypt;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,44 +10,32 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class JasyptConfig {
-
-    @Autowired(required = false)
     private JasyptProperties jasyptProperties;
 
     @Bean("jasyptStringEncryptor")
     @Primary
     @Profile("prod")
     public StringEncryptor prodEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-
-        config.setPassword(jasyptProperties.getPassword());
-        config.setAlgorithm("PBEWithMD5AndDES");
-        config.setKeyObtentionIterations("1000");
-        config.setPoolSize("1");
-        config.setProviderName("SunJCE");
-        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setStringOutputType("base64");
-
-        encryptor.setConfig(config);
-        return encryptor;
+        return encryptorSetting(jasyptProperties.getPassword());
     }
 
     @Bean("jasyptStringEncryptor")
     @Primary
     @Profile("dev")
     public StringEncryptor devEncryptor() {
-        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        return encryptorSetting("ark-back");
+    }
 
-        config.setPassword("ark-back");
+    public PooledPBEStringEncryptor encryptorSetting(final String password) {
+        final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        final SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
         config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
         config.setStringOutputType("base64");
-
         encryptor.setConfig(config);
         return encryptor;
     }

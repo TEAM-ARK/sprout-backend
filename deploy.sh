da1e803d6a506b4c8b70864f62dd865d71698deb
MinChul-Son <smc5236@naver.com>
2021-09-12 17:41:58 +0900

    refactor : Green 중지 및 배포

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 JAR_NAME="inflearn-clone-back.jar"
 
-echo "> 현재 구동중인 애플리케이션 확인 중..." >> /home/ubuntu/deploy.log
+echo "> Green 애플리케이션 확인 중..." >> /home/ubuntu/deploy.log
 
-CURRENT_PID=$(ps -ef | grep $JAR_NAME | grep -v grep | awk '{print $2}')
+CURRENT_PID=$(ps -ef | grep 8081 | grep -v grep | awk '{print $2}')
 
 echo "$CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
@@ -16,5 +16,5 @@ fi
 
 chmod -x /home/ubuntu/app/build/libs/$JAR_NAME
 
-nohup java -jar -Djasypt.encryptor.password="$JASYPT_PASSWORD" -Dspring.profiles.active=prod /home/ubuntu/app/build/libs/$JAR_NAME >> /dev/null &
+nohup java -jar -Djasypt.encryptor.password="$JASYPT_PASSWORD" -Dspring.profiles.active=prod /home/ubuntu/app/build/libs/$JAR_NAME --server.port=8081 >> /dev/null &
 echo "[$(date)] 배포 완료 !" >> /home/ubuntu/deploy.log

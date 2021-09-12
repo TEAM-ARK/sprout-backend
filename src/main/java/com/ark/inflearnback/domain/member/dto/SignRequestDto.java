39f49ecf1a1365f6208d0c38f4465fe50b115a0d
lsj8367 <lsj8367@naver.com>
2021-09-12 20:10:31 +0900

    remove: 주석 제거

--- src/main/java/com/ark/inflearnback/domain/member/dto/SignRequestDto.java
+++ src/main/java/com/ark/inflearnback/domain/member/dto/SignRequestDto.java
@@ -20,6 +20,8 @@ public class SignRequestDto {
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&*])(?!.*(.)\\1\\1\\1)[0-9a-zA-Z!@#$%&*]{12,32}$", message = "비밀번호는 공백 없이 영문/숫자/특수문자를 조합해 12~32자리여야 합니다.")
     private String password;
 
+    //TODO
+
     private SignRequestDto(final String email, final String password) {
         this.email = email;
         this.password = password;

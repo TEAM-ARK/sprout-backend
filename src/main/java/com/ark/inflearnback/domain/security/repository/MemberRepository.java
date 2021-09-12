952e3d79f494f4bc45cccb186113a0bc2a4d76c8
lsj8367 <lsj8367@naver.com>
2021-09-12 20:11:25 +0900

    feat: social 아이디 찾기 쿼리메소드 생성

--- src/main/java/com/ark/inflearnback/domain/security/repository/MemberRepository.java
+++ src/main/java/com/ark/inflearnback/domain/security/repository/MemberRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(final String email);
     boolean existsByEmail(final String email);
+    Optional<Member> findBySocialId(String socialId);
 }

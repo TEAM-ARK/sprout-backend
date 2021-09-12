d53bb20918ac061d62beb787abd76adbde7cd1c8
MinChul-Son <smc5236@naver.com>
2021-09-12 19:00:27 +0900

    feature : 무중단 배포
    1. 8081과 Nginx 연결 해제
    2. 커넥션 풀 Time Out를 위해 30초 대기
    3. 프로세스 죽이고 배포
    4. 다시 Nginx에 연결

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 JAR_NAME="inflearn-clone-back.jar"
+CONF_PATH="/etc/nginx/conf/upstream.conf"
 
 echo "> Green 애플리케이션 확인 중..." >> /home/ubuntu/deploy.log
 
@@ -8,6 +9,12 @@ echo "$CURRENT_PID"
 if [ -z $CURRENT_PID ]; then
         echo "> 현재 구동중인 애플리케이션이 없습니다 !" >> /home/ubuntu/deploy.log
 else
+        echo "> Green과 Nginx의 연결을 해제합니다 !" >> /home/ubuntu/deploy.log
+        sed -i "s/server 127.0.0.1:8081;/#server 127.0.0.1:8081;/g" $CONF_PATH
+
+        echo "> Connection Time Out까지 30초 대기 중 !" >> /home/ubuntu/deploy.log
+        sleep 30
+
         echo "> 구동중인 애플리케이션을 종료합니다 !" >> /home/ubuntu/deploy.log
         echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/deploy.log
         kill -9 $CURRENT_PID
@@ -18,3 +25,6 @@ chmod -x /home/ubuntu/app/build/libs/$JAR_NAME
 
 nohup java -jar -Djasypt.encryptor.password="$JASYPT_PASSWORD" -Dspring.profiles.active=prod /home/ubuntu/app/build/libs/$JAR_NAME --server.port=8081 >> /dev/null &
 echo "[$(date)] 배포 완료 !" >> /home/ubuntu/deploy.log
+
+echo "> Green과 Nginx를 다시 연결합니다 !" >> /home/ubuntu/deploy.log
+sed -i "s/#server 127.0.0.1:8081;/server 127.0.0.1:8081;/g" $CONF_PATH

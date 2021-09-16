2d79b714bae1f0252604d1c1461ede93a1187be9
lsj8367 <lsj8367@naver.com>
2021-09-16 22:18:53 +0900

    refactor: 생성자 오버로딩 추가 및 삭제

--- src/main/java/com/ark/inflearnback/configuration/http/model/form/HttpResponse.java
+++ src/main/java/com/ark/inflearnback/configuration/http/model/form/HttpResponse.java
@@ -29,13 +29,12 @@ public class HttpResponse<T> {
         return new HttpResponse<>(responseCode, responseMessage);
     }
 
-    public static <T> HttpResponse<T> of(final HttpStatus httpStatus,
-        final String responseMessage) {
-        return new HttpResponse<>(parseStr(httpStatus.value()), responseMessage);
-    }
-
     public static <T> HttpResponse<T> of(final HttpStatus httpStatus) {
         return new HttpResponse<>(parseStr(httpStatus.value()), httpStatus.getReasonPhrase());
+    }
+
+    public static <T> HttpResponse<T> of(final HttpStatus httpStatus, final T responseBody) {
+        return new HttpResponse<>(parseStr(httpStatus.value()), httpStatus.getReasonPhrase(), responseBody);
     }
 
     public static <T> HttpResponse<T> of(final HttpStatus httpStatus, final String responseMessage, final T responseBody) {

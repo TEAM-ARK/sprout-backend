c88216dda20487312db0cb353577aa4cb065d051
MinChul-Son <smc5236@naver.com>
2021-09-30 22:37:21 +0900

    refactor : 사용하지않는 import 라인 삭제

--- src/main/java/com/ark/inflearnback/configuration/redis/RedisConfig.java
+++ src/main/java/com/ark/inflearnback/configuration/redis/RedisConfig.java
@@ -7,7 +7,6 @@ import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration

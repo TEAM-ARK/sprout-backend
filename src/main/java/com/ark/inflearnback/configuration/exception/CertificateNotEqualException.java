13ef1ddd7409436d887d6776ef556a702933ddc6
lsj8367 <lsj8367@naver.com>
2021-10-02 16:44:38 +0900

    feat: email 같지 않을때, 인증번호 같지 않을때 예외처리

---
+++ src/main/java/com/ark/inflearnback/configuration/exception/CertificateNotEqualException.java
@@ -0,0 +1,12 @@
+package com.ark.inflearnback.configuration.exception;
+
+public class CertificateNotEqualException extends RuntimeException {
+
+    public CertificateNotEqualException() {
+    }
+
+    public CertificateNotEqualException(String message) {
+        super(message);
+    }
+
+}

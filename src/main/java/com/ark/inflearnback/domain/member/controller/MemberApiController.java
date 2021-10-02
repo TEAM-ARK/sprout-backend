c708320faab801812e8a03cacb232329f89a88dd
lsj8367 <lsj8367@naver.com>
2021-10-02 17:26:01 +0900

    feat: 비밀번호 인증번호 보내기, 인증번호 체크, 비밀번호 변경 로직 구현

--- src/main/java/com/ark/inflearnback/domain/member/controller/MemberApiController.java
+++ src/main/java/com/ark/inflearnback/domain/member/controller/MemberApiController.java
@@ -1,14 +1,16 @@
 package com.ark.inflearnback.domain.member.controller;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.OK;
 import com.ark.inflearnback.configuration.http.model.form.HttpResponse;
+import com.ark.inflearnback.domain.member.model.form.PasswordForm;
 import com.ark.inflearnback.domain.member.model.form.SignForm;
 import com.ark.inflearnback.domain.member.service.MemberService;
+import java.util.Map;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,12 +23,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
 
     private final MemberService memberService;
+    private final JavaMailSender javaMailSender;
 
     @PostMapping
     public ResponseEntity<?> signUp(@Valid @RequestBody final SignForm request) {
         memberService.signUp(request);
         return ResponseEntity.ok(
             HttpResponse.of(OK, "sign-up successful"));
+    }
+
+    @PostMapping("/informationMail")
+    public ResponseEntity<?> findPassword(@Valid @RequestBody final Map<String, String> requestMap) {
+        javaMailSender.send(memberService.informationEmail(requestMap.get("email")));
+        return ResponseEntity.ok(
+            HttpResponse.of(OK, "mail send successful"));
+    }
+
+    @PostMapping("/check")
+    public ResponseEntity<?> checkKey(@Valid @RequestBody final Map<String, String> requestMap) {
+        memberService.checkKey(requestMap.get("key"));
+        return ResponseEntity.ok(HttpResponse.of(OK, "certificate check successful"));
+    }
+
+    @PostMapping("/changePassword")
+    public ResponseEntity<?> changePassword(@Valid @RequestBody final PasswordForm passwordForm) {
+        memberService.changePassword(passwordForm);
+        return ResponseEntity.ok(HttpResponse.of(OK, "change password complete"));
     }
 
 }

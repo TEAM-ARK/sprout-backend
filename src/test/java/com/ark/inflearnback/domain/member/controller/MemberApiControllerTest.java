353e0fef85e0f8b53c7ffbd068efb7919ce9dbf8
lsj8367 <lsj8367@naver.com>
2021-10-02 16:45:28 +0900

    test: 메일 보내기 테스트 구현

--- src/test/java/com/ark/inflearnback/domain/member/controller/MemberApiControllerTest.java
+++ src/test/java/com/ark/inflearnback/domain/member/controller/MemberApiControllerTest.java
@@ -3,6 +3,7 @@ package com.ark.inflearnback.domain.member.controller;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
 import static com.epages.restdocs.apispec.Schema.schema;
 import static com.epages.restdocs.apispec.WebTestClientRestDocumentationWrapper.document;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
@@ -11,21 +12,26 @@ import static org.springframework.restdocs.webtestclient.WebTestClientRestDocume
 import static org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 import static org.springframework.web.reactive.function.BodyInserters.fromProducer;
 import com.ark.inflearnback.configuration.http.model.form.HttpResponse;
-import com.ark.inflearnback.domain.member.model.form.SignForm;
 import com.ark.inflearnback.configuration.security.model.entity.Member;
 import com.ark.inflearnback.configuration.security.model.entity.Role;
 import com.ark.inflearnback.configuration.security.repository.MemberRepository;
 import com.ark.inflearnback.configuration.security.repository.RoleRepository;
 import com.ark.inflearnback.configuration.security.type.RoleType;
+import com.ark.inflearnback.domain.member.model.form.SignForm;
+import com.ark.inflearnback.domain.member.utils.MailEnum;
 import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -45,6 +51,9 @@ class MemberApiControllerTest {
     private final MemberRepository memberRepository;
     private final RoleRepository roleRepository;
     private WebTestClient webTestClient;
+
+    @Autowired
+    private JavaMailSender javaMailSender;
 
     public MemberApiControllerTest(final ObjectMapper objectMapper, final PasswordEncoder passwordEncoder, final MemberRepository memberRepository, final RoleRepository roleRepository) {
         this.objectMapper = objectMapper;
@@ -169,6 +178,47 @@ class MemberApiControllerTest {
                                 fieldWithPath("responseMessage").description("응답메시지"),
                                 fieldWithPath("responseBody").description("응답바디"))
                             .build())));
+    }
+
+    @Test
+    void mailSender() {
+        final String email = "lsj8367@naver.com";
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(email);
+        message.setTo("dyctdy@gmail.com");
+        message.setSubject(MailEnum.AUTHENTICATION_TITLE.getValue());
+
+        message.setText("인증키는 ");
+        javaMailSender.send(message);
+    }
+
+    @Test
+    @DisplayName("이메일 인증키 보내기")
+    void informationMailTest() throws Exception {
+        final String email = "test@email.com";
+        final String password = passwordEncoder.encode("AASHFKHQWFQYW#qwhfgqwf123!");
+        final Role role = roleRepository.findByRoleType(RoleType.USER).get();
+        memberRepository.saveAndFlush(Member.of(email, password, role, null, null, false));
+
+        //given
+        Mono<String> request = Mono.just(objectMapper.writeValueAsString(Map.of(
+            "email", "test@email.com"
+        )));
+        String response = objectMapper.writeValueAsString(HttpResponse.of(OK, "mail send successful"));
+
+        //when
+        WebTestClient.ResponseSpec exchange = webTestClient.post()
+            .uri("/api/v1/member/informationMail")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON)
+            .body(fromProducer(request, String.class))
+            .exchange();
+
+        //then
+        exchange
+            .expectStatus().isOk()
+            .expectBody()
+            .json(response);
     }
 
 }

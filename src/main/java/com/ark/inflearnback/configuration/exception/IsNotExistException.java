fa51e15f371966476971f7cd83b2480da4b65d3a
lsj8367 <lsj8367@naver.com>
2021-10-24 13:24:10 +0900

    feat: 토큰 못찾을때의 사용자 정의 예외

---
+++ src/main/java/com/ark/inflearnback/configuration/exception/IsNotExistException.java
@@ -0,0 +1,9 @@
+package com.ark.inflearnback.configuration.exception;
+
+public class IsNotExistException extends RuntimeException {
+
+    public IsNotExistException(String message) {
+        super(message);
+    }
+
+}

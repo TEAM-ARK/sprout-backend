7d0223bdb058035049140cd865d076fa566a0c53
lsj8367 <lsj8367@naver.com>
2021-10-24 13:26:12 +0900

    feat: 1분마다 토큰 정리 스케줄러 구현

---
+++ src/main/java/com/ark/inflearnback/configuration/token/TokenScheduler.java
@@ -0,0 +1,21 @@
+package com.ark.inflearnback.configuration.token;
+
+import com.ark.inflearnback.domain.member.utils.TokenGenerator;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Slf4j
+@EnableScheduling
+@RequiredArgsConstructor
+public class TokenScheduler {
+
+    private final TokenGenerator tokenGenerator;
+
+    @Scheduled(fixedDelay = 1000 * 60)
+    public void cleanUp() {
+        log.info("Clean up expired token");
+        tokenGenerator.cleanUp();
+    }
+}

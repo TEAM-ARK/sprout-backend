c67d1d8996115d14059043c7a58b773bd0ebd041
MinChul-Son <smc5236@naver.com>
2021-11-17 15:17:11 +0900

    누락되었던 Redis Configuration 복구 (#171)
    * feat : redis 의존성 추가
    
    * feat : redis 설정 추가
    
    * feat : 애플리케이션에 redis 적용
    
    * fix : 무중단 배포 쉘 스크립트 수정
    
    - 실행권한을 빼앗는 "-"에서 주는 "+"로 변경
    
    * fix : 피드백 반영(final 추가)

---
+++ module-common/src/main/java/com/ark/sprout/redis/RedisConfiguration.java
@@ -0,0 +1,38 @@
+package com.ark.sprout.redis;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        final RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(lettuceConnectionFactory());
+        template.setDefaultSerializer(new StringRedisSerializer());
+        return template;
+    }
+}

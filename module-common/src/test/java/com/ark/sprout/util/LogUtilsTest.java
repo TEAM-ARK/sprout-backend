3b4b31154a70d4fedde068797ee292a7b6595cc6
shirohoo <shirohoo4102@gmail.com>
2021-11-18 23:11:26 +0900

    fix: LogUtilsTest 의 테스트 케이스를 수정했다

--- module-common/src/test/java/com/ark/sprout/util/LogUtilsTest.java
+++ module-common/src/test/java/com/ark/sprout/util/LogUtilsTest.java
@@ -3,95 +3,166 @@ package com.ark.sprout.util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.read.ListAppender;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
 
 class LogUtilsTest {
 
+    private Logger logger;
+    private ListAppender<ILoggingEvent> listAppender;
+
+    @BeforeEach
+    protected void given() {
+        listAppender = new ListAppender<>();
+        logger = (Logger) LoggerFactory.getLogger(this.getClass());
+        logger.addAppender(listAppender);
+        listAppender.start();
+    }
+
+    @AfterEach
+    protected void after() {
+        logger.detachAppender(listAppender);
+    }
+
     @Test
     void info_1() {
-        String infoMessage = LogUtils.info("infoMessage", createDummy());
-        assertThat(infoMessage).isEqualTo("--✅> [info_1] infoMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.info(logger, "infoMessage");
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--✅> [info_1] infoMessage ");
     }
 
     @Test
     void info_2() {
-        String infoMessage = LogUtils.info("infoMessage", createDummy());
-        assertThat(infoMessage).isEqualTo("--✅> [info_2] infoMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.info(logger, "infoMessage", createDummy());
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--✅> [info_2] infoMessage Dummy{id=1, name='dummy class'}");
     }
 
     @Test
     void warn_1() {
-        String infoMessage = LogUtils.warn("warnMessage", createDummy());
-        assertThat(infoMessage).isEqualTo("--⚠> [warn_1] warnMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.warn(logger, "warnMessage");
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--⚠> [warn_1] warnMessage ");
     }
 
     @Test
     void warn_2() {
-        String infoMessage = LogUtils.warn("warnMessage", createDummy());
-        assertThat(infoMessage).isEqualTo("--⚠> [warn_2] warnMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.warn(logger, "warnMessage", createDummy());
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--⚠> [warn_2] warnMessage Dummy{id=1, name='dummy class'}");
     }
 
     @Test
     void error_1() {
-        String errorMessage = LogUtils.error("errorMessage", createDummy());
-        assertThat(errorMessage).isEqualTo("--❌> [error_1] errorMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.error(logger, "errorMessage");
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--❌> [error_1] errorMessage ");
     }
 
     @Test
     void error_2() {
-        String errorMessage = LogUtils.error("errorMessage", createDummy());
-        assertThat(errorMessage).isEqualTo("--❌> [error_2] errorMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.error(logger, "errorMessage", createDummy());
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--❌> [error_2] errorMessage Dummy{id=1, name='dummy class'}");
     }
 
     @Test
     void custom_1() {
-        String customMessage = LogUtils.custom("❤", "customMessage", createDummy());
-        assertThat(customMessage).isEqualTo("--❤> [custom_1] customMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.custom(logger, "❤", "customMessage");
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--❤> [custom_1] customMessage ");
     }
 
     @Test
     void custom_2() {
-        String customMessage = LogUtils.custom("❤", "customMessage", createDummy());
-        assertThat(customMessage).isEqualTo("--❤> [custom_2] customMessage Dummy{id=1, name='dummy class'}");
+        // when
+        LogUtils.custom(logger, "❤", "customMessage", createDummy());
+
+        // then
+        List<ILoggingEvent> list = listAppender.list;
+        assertThat(list.get(0).getFormattedMessage()).isEqualTo("--❤> [custom_2] customMessage Dummy{id=1, name='dummy class'}");
     }
 
     @Test
     void nullPointerException() throws Exception {
         assertAll(
             () -> assertThatThrownBy(() -> {
-                LogUtils.info(null, createDummy());
+                LogUtils.info(null, "");
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.info("infoMessage", null);
+                LogUtils.info(logger, null, createDummy());
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.warn(null, createDummy());
+                LogUtils.info(logger, "", null);
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.warn("warnMessage", null);
+                LogUtils.warn(null, "");
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.error(null, createDummy());
+                LogUtils.warn(logger, null, createDummy());
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.error("errorMessage", null);
+                LogUtils.warn(logger, "", null);
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.custom(null, "customMessage", createDummy());
+                LogUtils.error(null, "");
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.custom("❤", null, createDummy());
+                LogUtils.error(logger, null, createDummy());
             }).isInstanceOf(NullPointerException.class),
 
             () -> assertThatThrownBy(() -> {
-                LogUtils.custom("❤", "customMessage", null);
+                LogUtils.error(logger, "", null);
+            }).isInstanceOf(NullPointerException.class),
+
+            () -> assertThatThrownBy(() -> {
+                LogUtils.custom(null, "", "");
+            }).isInstanceOf(NullPointerException.class),
+
+            () -> assertThatThrownBy(() -> {
+                LogUtils.custom(logger, null, "", createDummy());
+            }).isInstanceOf(NullPointerException.class),
+
+            () -> assertThatThrownBy(() -> {
+                LogUtils.custom(logger, "❤", null, createDummy());
+            }).isInstanceOf(NullPointerException.class),
+
+            () -> assertThatThrownBy(() -> {
+                LogUtils.custom(logger, "❤", "", null);
             }).isInstanceOf(NullPointerException.class)
         );
     }

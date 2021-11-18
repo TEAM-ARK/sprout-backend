9be74dab3a1eccdbac5b9fe6e09dca8c0c7a9b78
shirohoo <shirohoo4102@gmail.com>
2021-11-18 23:13:16 +0900

    fix: LogUtils의 구현을 변경했다
    - 중복 제거

--- module-common/src/main/java/com/ark/sprout/util/LogUtils.java
+++ module-common/src/main/java/com/ark/sprout/util/LogUtils.java
@@ -1,18 +1,19 @@
 package com.ark.sprout.util;
 
+import static java.util.Arrays.stream;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.text.MessageFormat;
 import java.util.Objects;
+import org.slf4j.Logger;
 
 public abstract class LogUtils {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
-    private static final String INFO_FORMAT = "--✅> [{0}] {1} {2}";
-    private static final String WARN_FORMAT = "--⚠> [{0}] {1} {2}";
-    private static final String ERROR_FORMAT = "--❌> [{0}] {1} {2}";
-    private static final String CUSTOM_FORMAT = "--{0}> [{1}] {2} {3}";
+    private static final String INFO_FORMAT = "--✅> [{}] {} {}";
+    private static final String WARN_FORMAT = "--⚠> [{}] {} {}";
+    private static final String ERROR_FORMAT = "--❌> [{}] {} {}";
+    private static final String CUSTOM_FORMAT = "--{}> [{}] {} {}";
 
     private static final int INVOKER = 2;
 
@@ -21,101 +22,99 @@ public abstract class LogUtils {
     /**
      * Called when the log level is info and the parameter does not exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param message log message
-     * @return formatted log message
      */
-    public static String info(final String message) {
-        Objects.requireNonNull(message);
-        return MessageFormat.format(INFO_FORMAT, invokerMethodName(), message, "");
+    public static void info(final Logger logger, final String message) {
+        requiredNonNulls(logger, message);
+        logger.info(INFO_FORMAT, invokerMethodName(), message, "");
     }
 
     /**
      * Called when the log level is info and the parameter does exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param message log message
      * @param param   log parameter
-     * @return formatted log message
      */
-    public static String info(final String message, final Object param) {
-        Objects.requireNonNull(message);
-        Objects.requireNonNull(param);
-        return MessageFormat.format(INFO_FORMAT, invokerMethodName(), message, toJsonString(param));
+    public static void info(final Logger logger, final String message, final Object param) {
+        requiredNonNulls(logger, message, param);
+        logger.info(INFO_FORMAT, invokerMethodName(), message, toJsonString(param));
     }
 
     /**
      * Called when the log level is warn and the parameter does not exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param message log message
-     * @return formatted log message
      */
-    public static String warn(final String message) {
-        Objects.requireNonNull(message);
-        return MessageFormat.format(WARN_FORMAT, invokerMethodName(), message, "");
+    public static void warn(final Logger logger, final String message) {
+        requiredNonNulls(logger, message);
+        logger.warn(WARN_FORMAT, invokerMethodName(), message, "");
     }
 
     /**
      * Called when the log level is warn and the parameter does exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param message log message
      * @param param   log parameter
-     * @return formatted log message
      */
-    public static String warn(final String message, final Object param) {
-        Objects.requireNonNull(message);
-        Objects.requireNonNull(param);
-        return MessageFormat.format(WARN_FORMAT, invokerMethodName(), message, toJsonString(param));
+    public static void warn(final Logger logger, final String message, final Object param) {
+        requiredNonNulls(logger, message, param);
+        logger.warn(WARN_FORMAT, invokerMethodName(), message, toJsonString(param));
     }
 
     /**
      * Called when the log level is error and the parameter does not exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param message log message
-     * @return formatted log message
      */
-    public static String error(final String message) {
-        Objects.requireNonNull(message);
-        return MessageFormat.format(ERROR_FORMAT, invokerMethodName(), message, "");
+    public static void error(final Logger logger, final String message) {
+        requiredNonNulls(logger, message);
+        logger.error(ERROR_FORMAT, invokerMethodName(), message, "");
     }
 
     /**
      * Called when the log level is error and the parameter does exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param message log message
      * @param param   log parameter
-     * @return formatted log message
      */
-    public static String error(final String message, final Object param) {
-        Objects.requireNonNull(message);
-        Objects.requireNonNull(param);
-        return MessageFormat.format(ERROR_FORMAT, invokerMethodName(), message, toJsonString(param));
+    public static void error(final Logger logger, final String message, final Object param) {
+        requiredNonNulls(logger, message, param);
+        logger.error(ERROR_FORMAT, invokerMethodName(), message, toJsonString(param));
     }
 
     /**
      * Called when the log level is not info and error and the parameter does not exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param emoji   emoji to be displayed first
      * @param message log message
-     * @return formatted log message
      */
-    public static String custom(final String emoji, final String message) {
-        Objects.requireNonNull(emoji);
-        Objects.requireNonNull(message);
-        return MessageFormat.format(CUSTOM_FORMAT, emoji, invokerMethodName(), message, "");
+    public static void custom(final Logger logger, final String emoji, final String message) {
+        requiredNonNulls(logger, emoji, message);
+        logger.info(CUSTOM_FORMAT, emoji, invokerMethodName(), message, "");
     }
 
     /**
      * Called when the log level is not info and error and the parameter does exist.
      *
+     * @param logger  org.slf4j.Logger
      * @param emoji   emoji to be displayed first
      * @param message log message
      * @param param   log parameter
-     * @return formatted log message
      */
-    public static String custom(final String emoji, final String message, final Object param) {
-        Objects.requireNonNull(emoji);
-        Objects.requireNonNull(message);
-        Objects.requireNonNull(param);
-        return MessageFormat.format(CUSTOM_FORMAT, emoji, invokerMethodName(), message, toJsonString(param));
+    public static void custom(final Logger logger, final String emoji, final String message, final Object param) {
+        requiredNonNulls(logger, emoji, message, param);
+        logger.info(CUSTOM_FORMAT, emoji, invokerMethodName(), message, toJsonString(param));
+    }
+
+    private static void requiredNonNulls(final Object... objects) {
+        stream(objects).forEach(Objects::requireNonNull);
     }
 
     private static String invokerMethodName() {

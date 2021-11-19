2ad8d37aa04c8a9b99a2eea84461778412194363
shirohoo <shirohoo4102@gmail.com>
2021-11-19 23:16:08 +0900

    :sparkles: 테스트 케이스 추가 - 작성자가 아닐 경우 스터디 게시글을 수정할 수 없다

--- module-community-study/src/test/java/com/ark/sprout/entity/StudyPostTest.java
+++ module-community-study/src/test/java/com/ark/sprout/entity/StudyPostTest.java
@@ -1,28 +1,47 @@
 package com.ark.sprout.entity;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.ark.sprout.form.NewPostForm;
 import com.ark.sprout.type.RoleType;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.authentication.BadCredentialsException;
 
 class StudyPostTest {
 
-    private Member fakeMember;
-    private NewPostForm PF1;
-    private NewPostForm PF2;
+    private Member FM1; // fakeMember1
+    private Member FM2; // fakeMember2
+    private NewPostForm PF1; // postForm1
+    private NewPostForm PF2; // postForm2
 
     @BeforeEach
     void setUp() {
         // given
         PF1 = NewPostForm.of("D2om1y", "s3I9AlN5FPjfuEGJzpw94nmL2EIyy64T2");
+
         PF2 = NewPostForm.of("AGJAG125", "78Yh5ivdO07v1cREGL0132XpMGX53P8udv5f6r473");
-        fakeMember = Member.builder()
-            .email("fakeMember@test.com")
+
+        FM1 = Member.builder()
+            .email("fakeMember1@test.com")
             .password("1234")
             .socialId("sUcaYN8j02udIqOuU")
             .registrationId("k76jI0Xake11M4mLgF")
+            .role(
+                Role.builder()
+                    .roleType(RoleType.USER)
+                    .deleted(false)
+                    .build()
+            )
+            .isSocial(true)
+            .build();
+
+        FM2 = Member.builder()
+            .email("fakeMember2@test.com")
+            .password("1234")
+            .socialId("mCfNfCoRjqlog")
+            .registrationId("sB1I76FgY5s82xzNWL")
             .role(
                 Role.builder()
                     .roleType(RoleType.USER)
@@ -37,21 +56,21 @@ class StudyPostTest {
     @Test
     void newPost() throws Exception {
         // when
-        StudyPost newPost = StudyPost.newPost(fakeMember, PF1);
+        StudyPost newPost = StudyPost.newPost(FM1, PF1);
 
         // then
         assertThat(newPost.getTitle()).isEqualTo(PF1.getTitle());
         assertThat(newPost.getContent()).isEqualTo(PF1.getContent());
-        assertThat(newPost.getWriter()).isEqualTo(fakeMember);
+        assertThat(newPost.getWriter()).isEqualTo(FM1);
     }
 
     @Test
     void edit() throws Exception {
         // given
-        StudyPost studyPost = StudyPost.newPost(fakeMember, PF1);
+        StudyPost studyPost = StudyPost.newPost(FM1, PF1);
 
         // when
-        List<EditHistory> editHistories = studyPost.edit(fakeMember, PF2);
+        List<EditHistory> editHistories = studyPost.edit(FM1, PF2);
 
         // then
         assertThat(studyPost.getTitle()).isEqualTo(PF2.getTitle());
@@ -61,6 +80,18 @@ class StudyPostTest {
         assertThat(editHistories.size()).isEqualTo(1);
         assertThat(editHistory.getTitle()).isEqualTo(PF1.getTitle());
         assertThat(editHistory.getContent()).isEqualTo(PF1.getContent());
+    }
+
+    @Test
+    void edit_exception() throws Exception {
+        // given
+        StudyPost studyPost = StudyPost.newPost(FM1, PF1);
+
+        // when, then
+        assertThatThrownBy(() -> {
+            studyPost.edit(FM2, PF2);
+        }).isInstanceOf(BadCredentialsException.class)
+            .hasMessage("Only the owner can !");
     }
 
 }

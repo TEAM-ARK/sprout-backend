509dbaa97dc5057106c94cb20480b313b4f86617
shirohoo <shirohoo4102@gmail.com>
2021-11-20 18:45:51 +0900

    :white_check_mark: 스터디 게시글 일급컬렉션의 테스트 코드를 변경했다

---
+++ module-community-study/src/test/java/com/ark/sprout/entity/EditHistoriesTest.java
@@ -0,0 +1,52 @@
+package com.ark.sprout.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Set;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EditHistoriesTest {
+
+    private EditHistories editHistories;
+
+    @BeforeEach
+    void setUp() {
+        editHistories = EditHistories.newHistories();
+    }
+
+    @Test
+    void edit() throws Exception {
+        // given
+        int newVersion = editHistories.newVersion();
+        EditHistory editHistory = EditHistory.of("title", "content", newVersion);
+
+        // when
+        Set<EditHistory> histories = editHistories.edit(editHistory);
+
+        // then
+        assertThat(histories.size()).isEqualTo(1);
+        assertThat(histories)
+            .extracting("title", "content", "version")
+            .contains(
+                Tuple.tuple(editHistory.getTitle(), editHistory.getContent(), editHistory.getVersion())
+            );
+    }
+
+    @Test
+    void newVersion() {
+        // given
+        editHistories.edit(EditHistory.of("title", "content", 3));
+        editHistories.edit(EditHistory.of("title", "content", 2));
+        editHistories.edit(EditHistory.of("title", "content", 4));
+        editHistories.edit(EditHistory.of("title", "content", 5));
+        editHistories.edit(EditHistory.of("title", "content", 1));
+
+        // when
+        int version = editHistories.newVersion();
+
+        // then
+        assertThat(version).isEqualTo(6);
+    }
+
+}

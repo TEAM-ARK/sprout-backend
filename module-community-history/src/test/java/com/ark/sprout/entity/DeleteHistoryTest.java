1ba63f7b636bb4405df8580fdeab4f6937726299
shirohoo <shirohoo4102@gmail.com>
2021-11-26 19:44:06 +0900

    :sparkles: 컨텐츠 삭제이력 테스트코드를 정의했다

---
+++ module-community-history/src/test/java/com/ark/sprout/entity/DeleteHistoryTest.java
@@ -0,0 +1,56 @@
+package com.ark.sprout.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.ark.sprout.type.ContentType;
+import com.ark.sprout.type.RoleType;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class DeleteHistoryTest {
+
+    private Member deletedBy;
+    private LocalDateTime deleteAt;
+
+    @BeforeEach
+    void setUp() {
+        deletedBy = Member.builder()
+            .email("email")
+            .password("password")
+            .socialId("socialId")
+            .registrationId("registrationId")
+            .role(Role.builder()
+                .roleType(RoleType.USER)
+                .deleted(false)
+                .build())
+            .isSocial(true)
+            .build();
+
+        deleteAt = LocalDate.of(2000, 12, 12).atStartOfDay();
+    }
+
+    @ParameterizedTest
+    @EnumSource(ContentType.class)
+    void ofQuestion(final ContentType contentType) {
+        assertThat(deleteHistoryOf(contentType))
+            .extracting("contentType", "contentId", "deleteBy", "deleteAt")
+            .contains(contentType, 1L, deletedBy, deleteAt);
+    }
+
+
+    private DeleteHistory deleteHistoryOf(final ContentType contentType) {
+        if (ContentType.QUESTION == contentType) {
+            return DeleteHistory.ofQuestion(contentType, 1L, deletedBy, deleteAt);
+        }
+        if (ContentType.STUDY == contentType) {
+            return DeleteHistory.ofStudy(contentType, 1L, deletedBy, deleteAt);
+        }
+        if (ContentType.FREE == contentType) {
+            return DeleteHistory.ofFree(contentType, 1L, deletedBy, deleteAt);
+        }
+        return DeleteHistory.ofComment(contentType, 1L, deletedBy, deleteAt);
+    }
+
+}

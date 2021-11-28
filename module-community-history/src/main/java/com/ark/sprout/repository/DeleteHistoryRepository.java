66ee9aee2295140b455532006a8f2e7fd89222b9
shirohoo <shirohoo4102@gmail.com>
2021-11-28 21:19:27 +0900

    :sparkles: 게시글 수정, 삭제내역 JPA 리파지토리를 추가했다

---
+++ module-community-history/src/main/java/com/ark/sprout/repository/DeleteHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.ark.sprout.repository;
+
+import com.ark.sprout.entity.DeleteHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeleteHistoryRepository extends JpaRepository<DeleteHistory, Long> {
+}

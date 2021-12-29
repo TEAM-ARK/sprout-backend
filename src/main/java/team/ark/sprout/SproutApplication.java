148bb8f07d8eb4647e038fa40d8c9cc63835b71c
shirohoo <shirohoo4102@gmail.com>
2021-12-29 21:22:53 +0900

    :sparkles: ObjectMapper Bean 등록

--- src/main/java/team/ark/sprout/SproutApplication.java
+++ src/main/java/team/ark/sprout/SproutApplication.java
@@ -1,11 +1,24 @@
 package team.ark.sprout;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SproutApplication {
     public static void main(String[] args) {
         SpringApplication.run(SproutApplication.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+        return objectMapper;
     }
 }

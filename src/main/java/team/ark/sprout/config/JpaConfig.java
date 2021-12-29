1bf8fc73bc3fdd87b1322c5c37a6cf0ae9689ed6
shirohoo <shirohoo4102@gmail.com>
2021-12-29 21:14:01 +0900

    :sparkles: JPA 설정파일 작성
    @EnableJpaAuditing

---
+++ src/main/java/team/ark/sprout/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package team.ark.sprout.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}

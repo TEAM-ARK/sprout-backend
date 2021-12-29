ebf226171b39675a2d1731567a3c85c0feb60efa
shirohoo <shirohoo4102@gmail.com>
2021-12-29 21:03:57 +0900

    :sparkles: P6Spy 설정 추가

---
+++ src/main/java/team/ark/sprout/config/P6SpyConfig.java
@@ -0,0 +1,13 @@
+package team.ark.sprout.config;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class P6SpyConfig {
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spyPrettySqlFormatter.class.getName());
+    }
+}

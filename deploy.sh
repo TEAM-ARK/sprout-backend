c827711ebaaa9401c8c9d96b6c2ca8f208c2b93c
shirohoo <shirohoo4102@gmail.com>
2022-01-06 22:06:09 +0900

    :fire: 빌드 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,22 @@
 BUILD_PATH="~/app/build/libs"
 JAR_NAME="sprout.jar"
 
-echo "> 배포를 시작합니다." >> /home/ubuntu/deploy.log
+echo "> 배포를 시작합니다." >>/home/ubuntu/deploy.log
 
 CURRENT_PID=$(ps -ef | grep $JAR_NAME | grep -v grep | awk '{print $2}')
-
-echo "> $CURRENT_PID 가 구동 중입니다!" >> /home/ubuntu/deploy.log
 if [ -z $CURRENT_PID ]; then
-        echo "> 현재 구동중인 애플리케이션이 없습니다 !" >> /home/ubuntu/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없습니다 !" >>/home/ubuntu/deploy.log
 else
-        echo "> 현재 구동중인 애플리케이션을 종료합니다 !" >> /home/ubuntu/deploy.log
-        echo "> kill -9 $CURRENT_PID" >> /home/ubuntu/deploy.log
-        kill -9 $CURRENT_PID
-        sleep 10
+  echo "> 현재 구동중인 애플리케이션 $CURRENT_PID 을 종료합니다 !" >>/home/ubuntu/deploy.log
+  kill -9 $CURRENT_PID
+  sleep 10
 fi
 
+echo "> 배포를 시작합니다 !" >>/home/ubuntu/deploy.log
+echo "> $BUILD_PATH/$JAR_NAME 에 실행 권한을 부여합니다 !" >>/home/ubuntu/deploy.log
 chmod +x $BUILD_PATH/$JAR_NAME
 
-nohup java -jar -Djasypt.encryptor.password="$JASYPT_PASSWORD" -Dspring.profiles.active=prod $BUILD_PATH/$JAR_NAME >> /dev/null &
+echo "> 애플리케이션을 실행합니다 !" >>/home/ubuntu/deploy.log
+nohup java -jar -Djasypt.encryptor.password="$JASYPT_PASSWORD" -Dspring.profiles.active=prod $BUILD_PATH/$JAR_NAME >>/dev/null &
 
-echo "[$(date)] 배포 완료 !" >> /home/ubuntu/deploy.log
+echo "[$(date)] 배포 완료 !" >>/home/ubuntu/deploy.log

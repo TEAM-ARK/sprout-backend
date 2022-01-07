147ebecd62329ac1b20949ca9ca35c079f9f392d
shirohoo <shirohoo4102@gmail.com>
2022-01-07 14:42:21 +0900

    :recycle: OAuth2SuccessHandler
    코드 리팩토링 및 예외 처리 추가

--- src/main/java/team/ark/sprout/common/config/extension/OAuth2SuccessHandler.java
+++ src/main/java/team/ark/sprout/common/config/extension/OAuth2SuccessHandler.java
@@ -5,31 +5,36 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.DefaultRedirectStrategy;
-import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
 import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import team.ark.sprout.adapter.in.web.GitHubAttributes;
 import team.ark.sprout.domain.account.Account;
 import team.ark.sprout.port.out.AccountRepository;
 
 @Component
-@RequiredArgsConstructor
-public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
     private final ObjectMapper objectMapper;
     private final AccountRepository accountRepository;
+    private final RequestCache requestCache;
+
+    public OAuth2SuccessHandler(ObjectMapper objectMapper, AccountRepository accountRepository) {
+        this.objectMapper = objectMapper;
+        this.accountRepository = accountRepository;
+        this.requestCache = new HttpSessionRequestCache();
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         updateOrSave(getOAuth2User(authentication));
-        redirectToOriginDestination(request, response);
+        redirectToOriginDestination(request, response, authentication);
     }
 
     private OAuth2User getOAuth2User(Authentication authentication) {
@@ -50,10 +55,20 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         return objectMapper.convertValue(oAuth2User.getAttributes(), GitHubAttributes.class);
     }
 
-    private void redirectToOriginDestination(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        RequestCache requestCache = new HttpSessionRequestCache();
-        SavedRequest savedRequest = requestCache.getRequest(request, response);
-        RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
-        redirectStrategy.sendRedirect(request, response, savedRequest.getRedirectUrl());
+    private void redirectToOriginDestination(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        SavedRequest savedRequest = this.requestCache.getRequest(request, response);
+        if (savedRequest == null) {
+            super.onAuthenticationSuccess(request, response, authentication);
+            return;
+        }
+        String targetUrlParameter = getTargetUrlParameter();
+        if (isAlwaysUseDefaultTargetUrl() || (targetUrlParameter != null && StringUtils.hasText(request.getParameter(targetUrlParameter)))) {
+            this.requestCache.removeRequest(request, response);
+            super.onAuthenticationSuccess(request, response, authentication);
+            return;
+        }
+        clearAuthenticationAttributes(request);
+        String targetUrl = savedRequest.getRedirectUrl();
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 }
